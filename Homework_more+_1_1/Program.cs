511092d2bfad6502061cf7acfd8a6282f447bfe5
ReneeShkipper <marinkagnome@gmail.com>
2023-11-20 09:06:41 +0300

    Add project files.

---
+++ Homework_more+_1_1/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Input number a");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Input number b");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Input number c");
+int c = int.Parse(Console.ReadLine());
+Console.WriteLine(nsd_three(a, b, c));
+
+static int nsd(int a, int b)
+{
+    int x;
+	if (b == 0)
+	{
+		return Math.Abs(a);
+	}
+	return nsd(b, a % b);
+}
+
+static	int nsd_three(int a, int b, int c)
+{
+	int for_two = nsd(a, b);
+	int for_three = nsd(for_two, c);
+	return for_three;
+}
+
